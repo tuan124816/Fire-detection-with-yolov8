0b5a0a25acd7ddcf77c3d0bda752ceeb08971044
Huy Tuan <87602751+tuan124816@users.noreply.github.com>
2023-10-18 18:23:59 +0700

    Update multithread_fire_detector.py

--- multithread_fire_detector.py
+++ multithread_fire_detector.py
@@ -78,59 +78,59 @@
 
 
 
-# from ultralytics import YOLO
-# import cv2
+from ultralytics import YOLO
+import cv2
 
-# class FireDetectionSystem:
-#     def __init__(self, model_path, camera_ip):
-#         self.model = YOLO(model_path)
-#         self.camera_ip = camera_ip
-#         self.fire_frame = 0
+class FireDetectionSystem:
+    def __init__(self, model_path, camera_ip):
+        self.model = YOLO(model_path)
+        self.camera_ip = camera_ip
+        self.fire_frame = 0
 
-#     def check_fire_detection(self, results):
-#         if len(results[0]) == 0:
-#             return False
-#         return True
+    def check_fire_detection(self, results):
+        if len(results[0]) == 0:
+            return False
+        return True
 
-#     def send_alarm_message(self):
-#         print(f"Fire detected from camera {self.camera_ip}! Sending alarm message...")
+    def send_alarm_message(self):
+        print(f"Fire detected from camera {self.camera_ip}! Sending alarm message...")
 
-#     def run(self):
-#         cap = cv2.VideoCapture(self.camera_ip)
-#         while True:
-#             ret, frame = cap.read()
-#             if not ret:
-#                 break
+    def run(self):
+        cap = cv2.VideoCapture(self.camera_ip)
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-#             results = self.model(frame)
-#             fire_detected = self.check_fire_detection(results)
+            results = self.model(frame)
+            fire_detected = self.check_fire_detection(results)
 
-#             if fire_detected:
-#                 for result in results:
-#                     coordinates = result.boxes.xyxy[0].numpy()
-#                     x_min, y_min, x_max, y_max = map(int, coordinates)
-#                     cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color=(255, 0, 0))
-#                 self.fire_frame += 1
-#             else:
-#                 self.fire_frame = 0
+            if fire_detected:
+                for result in results:
+                    coordinates = result.boxes.xyxy[0].numpy()
+                    x_min, y_min, x_max, y_max = map(int, coordinates)
+                    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color=(255, 0, 0))
+                self.fire_frame += 1
+            else:
+                self.fire_frame = 0
 
-#             if self.fire_frame >= 12:
-#                 self.send_alarm_message()
+            if self.fire_frame >= 12:
+                self.send_alarm_message()
 
-#             cv2.imshow('fire detection', frame)
+            cv2.imshow('fire detection', frame)
 
-#             key = cv2.waitKey(1)
-#             if key == 27:
-#                 break
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
 
-#         cap.release()
-#         cv2.destroyAllWindows()
+        cap.release()
+        cv2.destroyAllWindows()
 
-# if __name__ == "__main__":
-#     model_path = "/Users/huytuannguyen/Desktop/FPT/FAIC/FPTAI/FireDetectionYOLOv8/runs/detect/train/weights/best.pt"
-#     camera_ip = 0
-#     fire_detection_system = FireDetectionSystem(model_path, camera_ip)
-#     fire_detection_system.run()
+if __name__ == "__main__":
+    model_path = "/Users/huytuannguyen/Desktop/FPT/FAIC/FPTAI/FireDetectionYOLOv8/runs/detect/train/weights/best.pt"
+    camera_ip = 0
+    fire_detection_system = FireDetectionSystem(model_path, camera_ip)
+    fire_detection_system.run()
 
 
 
@@ -216,74 +216,74 @@
 
 
 
-from ultralytics import YOLO
-import cv2
-import threading
+# from ultralytics import YOLO
+# import cv2
+# import threading
 
-class FireDetectionSystem:
-    def __init__(self, model_path, camera_ips):
-        self.model = YOLO(model_path)
-        self.camera_ips = camera_ips
-        self.fire_frames = {camera_ip: 0 for camera_ip in camera_ips}
-        self.threads = []
+# class FireDetectionSystem:
+#     def __init__(self, model_path, camera_ips):
+#         self.model = YOLO(model_path)
+#         self.camera_ips = camera_ips
+#         self.fire_frames = {camera_ip: 0 for camera_ip in camera_ips}
+#         self.threads = []
 
-    def check_fire_detection(self, results):
-        if len(results[0]) == 0:
-            return False
-        return True
+#     def check_fire_detection(self, results):
+#         if len(results[0]) == 0:
+#             return False
+#         return True
 
-    def send_alarm_message(self, camera_ip):
-        print(f"Fire detected from camera {camera_ip}! Sending alarm message...")
+#     def send_alarm_message(self, camera_ip):
+#         print(f"Fire detected from camera {camera_ip}! Sending alarm message...")
 
-    def process_camera(self, camera_ip):
-        cap = cv2.VideoCapture(camera_ip)
-        cv2.namedWindow(f'Fire detection - Camera {camera_ip}', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow(f'Fire detection - Camera {camera_ip}', 640, 480)
+#     def process_camera(self, camera_ip):
+#         cap = cv2.VideoCapture(camera_ip)
+#         cv2.namedWindow(f'Fire detection - Camera {camera_ip}', cv2.WINDOW_NORMAL)
+#         cv2.resizeWindow(f'Fire detection - Camera {camera_ip}', 640, 480)
 
-        while True:
-            ret, frame = cap.read()
-            if frame is None:
-                # Handle the case where the frame is not available (e.g., camera disconnected)
-                print(f"Camera {camera_ip} is not providing valid frames.")
-            break
-            if not ret:
-                break
+#         while True:
+#             ret, frame = cap.read()
+#             if frame is None:
+#                 # Handle the case where the frame is not available (e.g., camera disconnected)
+#                 print(f"Camera {camera_ip} is not providing valid frames.")
+#             break
+#             if not ret:
+#                 break
 
-            results = self.model(frame)
-            fire_detected = self.check_fire_detection(results)
+#             results = self.model(frame)
+#             fire_detected = self.check_fire_detection(results)
 
-            if fire_detected:
-                for result in results:
-                    coordinates = result.boxes.xyxy[0].numpy()
-                    x_min, y_min, x_max, y_max = map(int, coordinates)
-                    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color=(255, 0, 0))
-                self.fire_frames[camera_ip] += 1
-            else:
-                self.fire_frames[camera_ip] = 0
+#             if fire_detected:
+#                 for result in results:
+#                     coordinates = result.boxes.xyxy[0].numpy()
+#                     x_min, y_min, x_max, y_max = map(int, coordinates)
+#                     cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color=(255, 0, 0))
+#                 self.fire_frames[camera_ip] += 1
+#             else:
+#                 self.fire_frames[camera_ip] = 0
 
-            if self.fire_frames[camera_ip] >= 12:
-                self.send_alarm_message(camera_ip)
+#             if self.fire_frames[camera_ip] >= 12:
+#                 self.send_alarm_message(camera_ip)
 
-            cv2.imshow(f'Fire detection - Camera {camera_ip}', frame)
+#             cv2.imshow(f'Fire detection - Camera {camera_ip}', frame)
 
-            key = cv2.waitKey(1)
-            if key == 27:
-                break
+#             key = cv2.waitKey(1)
+#             if key == 27:
+#                 break
 
-        cap.release()
-        cv2.destroyWindow(f'Fire detection - Camera {camera_ip}')
+#         cap.release()
+#         cv2.destroyWindow(f'Fire detection - Camera {camera_ip}')
 
-    def run(self):
-        for camera_ip in self.camera_ips:
-            thread = threading.Thread(target=self.process_camera, args=(camera_ip,))
-            self.threads.append(thread)
-            thread.start()
+#     def run(self):
+#         for camera_ip in self.camera_ips:
+#             thread = threading.Thread(target=self.process_camera, args=(camera_ip,))
+#             self.threads.append(thread)
+#             thread.start()
 
-        for thread in self.threads:
-            thread.join()
+#         for thread in self.threads:
+#             thread.join()
 
-if __name__ == "__main__":
-    model_path = "runs/detect/train/weights/best.pt"
-    camera_ips = [0, 'ultralytics/yolo/v8/detect/forestfire4.mp4']  # Replace with your camera IPs
-    fire_detection_system = FireDetectionSystem(model_path, camera_ips)
-    fire_detection_system.run()
+# if __name__ == "__main__":
+#     model_path = "runs/detect/train/weights/best.pt"
+#     camera_ips = [0, 'ultralytics/yolo/v8/detect/forestfire4.mp4']  # Replace with your camera IPs
+#     fire_detection_system = FireDetectionSystem(model_path, camera_ips)
+#     fire_detection_system.run()
